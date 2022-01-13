01b958841a09845d756728d01f82629c71153b5d
Arnav Sharma <arnav1776@gmail.com>
2022-01-13 17:08:57 +0530

    functions

---
+++ Js1/functions.js
@@ -0,0 +1,13 @@
+function sayHello(name) {
+    console.log(`Hello, ${name}. How are you`);
+}
+
+// sayHello("xyz");
+
+function Hello() {
+    return "Hi";
+}
+
+var greetings = Hello();
+
+console.log(greetings);
