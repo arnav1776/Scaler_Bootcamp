0e4362546f5d5c72ad177b44458a32b1721706a6
Arnav Sharma <arnav1776@gmail.com>
2022-01-16 18:19:47 +0530

    array

---
+++ Js2/array.js
@@ -0,0 +1,26 @@
+var countries = ["India", "USA", "Japan", "Russia"];
+
+var states = new Array("Rajasthan", "Assam", "Uttar Pradesh");
+
+// console.log(states[0]);
+
+// console.log(states.length);
+states[2] = "Punjab";
+// console.log(states);
+
+var user = ["xyz", "xyz@gmail.com", 3, 34, true];
+// console.log(user);
+
+user.pop();
+// console.log(user);
+user.unshift("NEW VALUE");
+// console.log(user);
+user.shift();
+console.log(user);
+
+console.log(user.indexOf("xyz"));
+
+console.log(Array.from("xyz"));
+
+
+
