c1f3a2128bdba514c181ae762145b9ec88547fc3
Arnav Sharma <arnav1776@gmail.com>
2022-01-19 20:06:30 +0530

    while-loop

--- Js2/loops.js
+++ Js2/loops.js
@@ -9,5 +9,17 @@ const myStates = [
 for (let i = 0; i <= myStates.length; i++) {
         if(typeof myStates[i] !== 'string') continue;
     console.log(myStates[i]);
-    
 }
+
+let i = 0;
+
+while(i < myStates.length) {
+    console.log(myStates[i]);
+    i++;
+}
+
+do {
+    console.log(i);
+    i++;
+} while(i < 10);
+
