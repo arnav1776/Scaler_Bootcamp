91e89625477c4d356de637487337338886d1c598
Arnav Sharma <arnav1776@gmail.com>
2022-01-24 18:48:45 +0530

    proj3

---
+++ Js5/proj3/script.js
@@ -0,0 +1,18 @@
+const courses = [
+    {
+        name: "Complete ReactJS Course",
+        price: "2.3"
+    },
+    {
+        name: "Complete Angular Course",
+        price: "2.1"
+    },
+    {
+        name: "Complete MERN Course",
+        price: "2.7"
+    },
+    {
+        name: "Complete C++ Course",
+        price: "2.8"
+    },
+]
