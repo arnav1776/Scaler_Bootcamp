5914b8dc75238e77d655e1f1468c485663da7d1f
Arnav Sharma <arnav1776@gmail.com>
2022-01-25 19:01:51 +0530

    strings

--- Js6/scope.js
+++ Js6/scope.js
@@ -6,3 +6,15 @@ if (true) {
 
 console.log(lastName);
 
+var Name = "xyz";
+var fullName = `${Name} abc`;
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("abc"));
+console.log(fullName.toUpperCase());
+
+
+
+
+
