8ff4cc5fa1839f478e27080473ad9e09f18e9354
Arnav Sharma <arnav1776@gmail.com>
2022-01-27 17:37:01 +0530

    private props

--- Js6/classes.js
+++ Js6/classes.js
@@ -3,17 +3,23 @@ class User {
         this.name = name;
         this.email = email;
     }
-    courseList = [];
+    #courseList = [];
 
     getInfo(){
         return {name: this.name, emai: this.email};
     }
     enrollCourse(name){
-        this.courseList.push(name);
+        this.#courseList.push(name);
     }
     getCourseList() {
-        return this.courseList;
+        return this.#courseList;
     }
 }
 
 module.exports = User;
+
+const sam = new User("sam", "sam@gmail.com")
+console.log(sam.getInfo());
+sam.enrollCourse("Angular Bootcamp");
+console.log(sam.getCourseList());
+console.log(sam.courseList);
